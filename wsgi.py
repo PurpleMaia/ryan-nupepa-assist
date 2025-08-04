2f81c3d32920635e2fa51d435496614e778e0392
David Pickett <pickettd@gmail.com>
2025-08-03 14:53:13 -1000

    Make port optionally configured by env var

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from app import app
 from waitress import serve
+import os
+
+PORT = os.getenv("PORT",54311)
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=54311)
+    serve(app, host="0.0.0.0", port=PORT)
